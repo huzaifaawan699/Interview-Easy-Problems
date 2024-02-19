4060dcad90f16238f1e3103551c78de712ae7cd8
Huzaifa Fiaz <119066287+huzaifaawan699@users.noreply.github.com>
2024-02-19 12:14:43 +0500

    Create Find Missing Number in Array.cpp

---
+++ Find Missing Number in Array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int missingarray(int arr[],int n){
+int total_sum=(n*(n+1))/2;
+
+  int array_sum=0;
+  for(int i=0;i<n;i++)
+    {
+      sum+=arr[i];
+    }
+  return total_sum - array_sum;
+  
+}
+int main(){
+int arr[] ={0,1,2,3,5,6,7};
+  int n=sizeof (arr)/sizeof(arr[0]);
+  cout<<"The Missing Number is :"<<missingarray(arr,n)<<endl;
+  return 0;
+  
+}
